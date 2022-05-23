e40280fd60475699946900ffb0b34ae1004d83e6
Wallace <wallaceassisadm@gmail.com>
2022-05-23 09:36:28 -0300

    atualizado

--- Main/src/ExeScanner.java
+++ Main/src/ExeScanner.java
@@ -6,12 +6,22 @@ public class ExeScanner {
         Scanner scanner = new Scanner(System.in);
         String nome;
         String sobrenome;
+        Integer dia;
+        Integer mes;
+        Integer ano;
+
         System.out.println("Entre com o seu nome: ");
         nome = scanner.nextLine();
         System.out.println("Entre com o seu sobrenome: ");
         sobrenome = scanner.nextLine();
+        System.out.println("Em que dia você nasceu: ");
+        dia = scanner.nextInt();
+        System.out.println("E o mês: ");
+        mes = scanner.nextInt();
+        System.out.println("E o ano: ");
+        ano = scanner.nextInt();
         System.out.println("Bem vindo " + nome + " "+ sobrenome);
-
+        System.out.println("Nascido em "+ dia +"/"+ mes + "/"+ ano);
         scanner.close();
 
     }
